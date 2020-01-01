7d9372e272f106741929cf21e8985673bb38fa7e
congdoan6997 <congdoan6997@gmail.com>
2020-01-01 23:01:23 +0300

    jwt

--- api_example/settings.py
+++ api_example/settings.py
@@ -123,6 +123,9 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
 
-   'DEFAULT_PERMISSION_CLASSES': ( 'rest_framework.permissions.IsAuthenticatedOrReadOnly', ),
+   'DEFAULT_PERMISSION_CLASSES': ( 'rest_framework.permissions.IsAuthenticated', ),
+   'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 
 }
